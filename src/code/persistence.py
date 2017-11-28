e4c5e85c8ad4927ba5e33a65b99afd8b4461229f
Wilder Rodrigues <wilder.rodrigues@ekholabs.ai>
2017-11-28 06:56:39 +0100

    Add write operations to store DataFrames as files.

---
+++ src/code/persistence.py
@@ -0,0 +1,33 @@
+"""
+    Organisation: ekholabs
+    Author: wilder.rodrigues@ekholabs.ai
+"""
+
+from utils import dataset as ds
+
+'''
+    Let's start with loading out 'scientists' dataset.
+'''
+scientists = ds.load_scientists()
+
+def save_to_pickle():
+    scientists.to_pickle('scientests.pickle')
+
+
+def save_to_csv():
+    scientists.to_csv('scientests.csv')
+
+
+def save_to_tsv():
+    scientists.to_csv('scientests.tsv', sep = '\t')
+
+
+def save_to_excel():
+    scientists.to_excel('scientists.xlsx', index = False)
+
+
+if __name__ == '__main__':
+    save_to_pickle()
+    save_to_csv()
+    save_to_tsv()
+    save_to_excel()
